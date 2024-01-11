69482e8d0f9707e39ed8684f32be34a22b3a0e44
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-12 02:24:08 +0300

    RELOAD

--- products/views.py
+++ products/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from products.categories_list import first_categories
 
 
 def home(request):
-    return render(request, 'products/base.html')
+    context = {'categories': first_categories}
+    return render(request, 'products/base.html', context)
+
+
+def favorities_products(request):
+    context = {}
+    return render(request, 'products/favorites_products.html', context=context)
