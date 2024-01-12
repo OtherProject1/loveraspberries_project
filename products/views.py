9cac16834f6a4e4c4d965ad7aecab9c4a6a4dd8d
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-12 15:53:25 +0300

    New view for base

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from products.categories_list import first_categories
+from products.categories_list import first_categories, navbar_list
 
 
 def home(request):
-    context = {'categories': first_categories}
+    context = {'categories': first_categories, 'navbar': navbar_list}
     return render(request, 'products/base.html', context)
 
 
