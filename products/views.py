6a7075b0bcad32c6519a4cb17e2442d7fe54ad9e
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-14 00:42:44 +0300

    Finish base

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from products.categories_list import first_categories, navbar_list
+from products.categories_list import first_categories, navbar_list, footer_list
 
-context = {'categories': first_categories, 'navbar': navbar_list}
+context = {'categories': first_categories, 'navbar': navbar_list, 'footer': footer_list}
 def home(request):
     return render(request, 'products/base.html', context)
 
