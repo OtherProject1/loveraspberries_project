ded8b87ddce7fc920c50a658ee2ed6ba21f92b94
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-13 13:19:20 +0300

    New function in views.py

--- products/views.py
+++ products/views.py
@@ -4,3 +4,6 @@ from products.categories_list import first_categories, navbar_list
 context = {'categories': first_categories, 'navbar': navbar_list}
 def home(request):
     return render(request, 'products/base.html', context)
+
+def favourites_products(request):
+    return render(request, 'products/favourites_products.html', context)
