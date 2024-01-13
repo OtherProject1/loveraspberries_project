c28ea92a76ef6b7591cc22988cb3d5cb2e066dc0
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-13 13:24:27 +0300

    Check view

--- products/views.py
+++ products/views.py
@@ -5,8 +5,3 @@ from products.categories_list import first_categories, navbar_list
 def home(request):
     context = {'categories': first_categories, 'navbar': navbar_list}
     return render(request, 'products/base.html', context)
-
-
-def favorities_products(request):
-    context = {}
-    return render(request, 'products/favorites_products.html', context=context)
