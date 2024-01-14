a84e40bd4df190ffffd8303d4fdbff25fead1b74
Vadim Kolbik <vkolbik46@gmail.com>
2024-01-14 12:02:10 +0300

    just commit number 1

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('profile/favourites_products/', favorities_products, name='favorites'),
+    path('profile/', profile, name='profile'),
+    path('profile/shopping_cart/', shopping_cart, name='shopping_cart'),
+    path('profile/delivery/', delivery, name='delivery'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,3 +6,15 @@ def home(request):
 def favorities_products(request):
     context = {}
     return render(request, 'products/favorites_products.html', context=context)
+
+def profile(request):
+    context = {'is_auth': True}
+    return render(request, 'products/profile.html', context)
+
+def shopping_cart(request):
+    context = {}
+    return render(request, 'products/shopping_cart.html', context)
+
+def delivery(request):
+    context = {}
+    return render(request, 'products/delivery.html', context)
