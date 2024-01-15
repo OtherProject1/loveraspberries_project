875504ef95666a13b3d093d90928f15b11ba7c51
SinShyrika <dmitry.sereda.d@gmail.com>
2024-01-15 23:57:40 +0300

    Add payments_method html

--- loveraspberries/urls.py
+++ loveraspberries/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('', include('products.urls', namespace='products'))
+    path('', include('products.urls', namespace='products')),
+
 ]
 
 if settings.DEBUG:
