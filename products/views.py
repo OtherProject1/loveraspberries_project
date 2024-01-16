85784985b93b0da644f963d4bd286285edb57567
Vadim Kolbik <vkolbik46@gmail.com>
2024-01-16 23:10:27 +0300

    correct_templates

--- products/views.py
+++ products/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
-from products.categories_list import first_categories, navbar_list, footer_list, cards, payment_list
+from products.categories_list import first_categories, navbar_list, footer_list, cards, payment_list, favourite_cards
 
-context = {'categories': first_categories, 'navbar': navbar_list, 'footer': footer_list, 'logo': payment_list}
+context = {'title': 'LoveRaspberry','categories': first_categories, 'navbar': navbar_list, 'footer': footer_list, 'logo': payment_list}
 
 
 def home(request):
     context['cards'] = cards
+    context['title'] = 'LoveRaspberry'
     return render(request, 'products/main.html', context)
 
-
-def favorities_products(request):
+def favourites_products(request):
+    context['favorites_products'] = favourite_cards
+    context['title'] = 'Избранное'
     return render(request, 'products/favourites_products.html', context)
-
 
 def profile(request):
     context['is_auth'] = True
+    context['title'] = 'Профиль'
     return render(request, 'products/profile.html', context)
 
 
 def shopping_cart(request):
+    context['title'] = 'Корзина'
     return render(request, 'products/shopping_cart.html', context)
 
 
 def delivery(request):
     context['is_auth'] = True
+    context['title'] = 'Доставки'
     return render(request, 'products/delivery.html', context)
-
-
-def favourites_products(request):
-    return render(request, 'products/favourites_products.html', context)
 
 
 def payment_methods(request):
