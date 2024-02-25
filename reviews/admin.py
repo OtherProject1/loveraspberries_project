af0c2d6998e45dc532cb84b8e2e606810df8ec6c
Vadim Kolbik <vkolbik46@gmail.com>
2024-02-25 13:18:16 +0300

    just for pull

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user', 'rating', 'date']
+    list_display_links = ['product', 'user', 'rating', 'date']
