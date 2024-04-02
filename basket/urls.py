b8375642ad7bc6d985211de8df6f537e8960d700
Vadim Kolbik <vkolbik46@gmail.com>
2024-04-02 20:30:46 +0300

    to finish for ajax basket and ajax favorites

--- basket/urls.py
+++ basket/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path('add_product_to_favorites/<int:product_id>/', add_product_to_favorites, name='add_product_to_favorites'),
     path('selected_for_purchase/<int:basket_id>/', selected_for_purchase, name='selected_for_purchase'),
     path('favorites/', FavoritesView.as_view(), name='favorites'),
-    # path('basket/', BasketView.as_view(), name='basket'),
-    path('basket/', basket_products, name='basket'),
+    path('basket/', BasketView.as_view(), name='basket'),
+    # path('basket/', basket_products, name='basket'),
     path('ajax/add_product_to_basket/<int:product_id>/', ajax_add_product_to_basket, name='ajax_add_product_to_basket'),
     path('ajax/minus_product_to_basket/<int:product_id>/', ajax_minus_product_to_basket, name='ajax_minus_product_to_basket'),
+    path('ajax/delete_product_from_basket/<int:product_id>/', ajax_delete_product_from_basket, name='ajax_minus_product_to_basket'),
+    path('ajax/ajax_add_product_to_favorites/<int:product_id>/', ajax_add_product_to_favorites, name='ajax_add_product_to_favorites'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -9,7 +9,8 @@ from django.urls import reverse
 from users.forms import UserLoginForm
 from .models import Basket, Favorites
 from products.models import Product
-
+from django.db.models.aggregates import Sum
+from django.db.models.expressions import F
 '''
 Твой вариант
 '''
@@ -45,23 +46,25 @@ from products.models import Product
 #         1]:
 #         product_in_favorites[0].delete()
 #     return HttpResponseRedirect(request.META['HTTP_REFERER'])
-# class BasketView(BaseMixin, ListView):
-#     model = Basket
-#     template_name = 'basket/basket.html'
-#     context_object_name = 'basket_products'
-#
-#     def get_queryset(self) -> QuerySet[Any]:
-#         return Basket.objects.filter(user=self.request.user).select_related('product')
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['basket_products_new'] = Basket.objects.filter(user=self.request.user)
-#         context['favorites_products'] = list(
-#             *list(zip(*Favorites.objects.filter(user=self.request.user).values_list('product'))))
-#         context['cost_selected_basket_products'] = context['basket_products'].filter(selected_for_purchase=1).aggregate(
-#             basket_cost=Sum('product__price'), basket_count_products=Sum('quantity'))
-#         print(context['favorites_products'])
-#         return context
+
+
+class BasketView(BaseMixin, ListView):
+    model = Basket
+    template_name = 'basket/basket.html'
+    context_object_name = 'basket_products'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Basket.objects.filter(user=self.request.user).select_related('product')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'StuffStore - Корзина'
+        context['basket_products_new'] = Basket.objects.filter(user=self.request.user)
+        context['favorites_products'] = list(
+            *list(zip(*Favorites.objects.filter(user=self.request.user).values_list('product'))))
+        context['cost_selected_basket_products'] = context['basket_products'].filter(selected_for_purchase=1).aggregate(
+            basket_cost=Sum(F('product__price')*F('quantity')), basket_count_products=Sum('quantity'))
+        return context
 '''
 Попробую написать функцию добавления товара немного по другому
 '''
@@ -82,9 +85,13 @@ def ajax_add_product_to_basket(request, product_id):
         basket.quantity += 1
         basket.save()
         response['basket_product_quantity'] = basket.quantity
-    response['basket_counter'] = Basket.objects.filter(user=request.user).count()
+    basket = Basket.objects.filter(user=request.user)
+    response['basket_counter'] = basket.count()
     response['add_product_to_basket'] = reverse('basket:ajax_add_product_to_basket', args=[product_id, ])
     response['minus_product_to_basket'] = reverse('basket:ajax_minus_product_to_basket', args=[product_id, ])
+    response['total_basket_quantity_and_cost'] = basket.filter(selected_for_purchase=1).aggregate(
+            basket_cost=Sum(F('product__price')*F('quantity')), basket_quantity=Sum('quantity'))
+    response['product_sum'] = product_in_basket[0].sum()
     return JsonResponse(response)
 
 
@@ -99,13 +106,44 @@ def ajax_minus_product_to_basket(request, product_id):
         basket.quantity -= 1
         basket.save()
         response['basket_product_quantity'] = basket.quantity
+        response['product_sum'] = product_in_basket[0].sum()
         if product_in_basket[0].quantity == 0:
             basket.delete()
+    basket = Basket.objects.filter(user=request.user)
     response['basket_counter'] = Basket.objects.filter(user=request.user).count()
     response['add_product_to_basket'] = reverse('basket:ajax_add_product_to_basket', args=[product_id, ])
     response['minus_product_to_basket'] = reverse('basket:ajax_minus_product_to_basket', args=[product_id, ])
+    response['total_basket_quantity_and_cost'] = basket.filter(selected_for_purchase=1).aggregate(
+            basket_cost=Sum(F('product__price')*F('quantity')), basket_quantity=Sum('quantity'))
     return JsonResponse(response)
 
+@login_required()
+def ajax_delete_product_from_basket(request, product_id):
+    response = {'success': True,
+                'product_id': product_id, }
+    product = Product.objects.get(pk=product_id)
+    product_in_basket = Basket.objects.filter(user=request.user, product=product)
+    Basket.objects.get(pk=product_in_basket[0].pk).delete()
+    basket = Basket.objects.filter(user=request.user)
+    response['basket_counter'] = Basket.objects.filter(user=request.user).count()
+    response['total_basket_quantity_and_cost'] = basket.filter(selected_for_purchase=1).aggregate(
+            basket_cost=Sum(F('product__price')*F('quantity')), basket_quantity=Sum('quantity'))
+    return JsonResponse(response)
+
+
+@login_required
+def ajax_add_product_to_favorites(request, product_id):
+    response = {'success': True,
+                'product_id': product_id, }
+    product = Product.objects.get(pk=product_id)
+    product_in_favorites = Favorites.objects.filter(user=request.user, product=product)
+    if product_in_favorites.exists():
+        product_in_favorites.last().delete()
+        response['add_product'] = False
+    else:
+        Favorites.objects.create(user=request.user, product=product)
+        response['add_product'] = True
+    return JsonResponse(response)
 
 @login_required
 def add_product_to_basket(request, product_id):
